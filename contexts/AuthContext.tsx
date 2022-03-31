ba24cca7bfb72809ec0baad9f2515d8d77fbac62
amanda-santos <amandasf0200@gmail.com>
2022-03-31 19:06:11 -0300

    ✨ Save token and login

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,4 +1,13 @@
-import { createContext, ReactNode } from "react";
+import Router from "next/router";
+import { parseCookies, setCookie } from "nookies";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { api } from "../services/api";
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+};
 
 type SignInCredentials = {
   email: string;
@@ -7,6 +16,7 @@ type SignInCredentials = {
 
 type AuthContextData = {
   signIn(credentials: SignInCredentials): Promise<void>;
+  user: User;
   isAuthenticated: boolean;
 };
 
@@ -17,14 +27,59 @@ type AuthProviderProps = {
 export const AuthContext = createContext({} as AuthContextData);
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
-  const isAuthenticated = false;
+  const [user, setUser] = useState<User>(null);
+  const isAuthenticated = !!user;
+
+  useEffect(() => {
+    const { "ignite-next-auth.token": token } = parseCookies();
+
+    if (token) {
+      api.get("/me").then((response) => {
+        const { email, permissions, roles } = response.data;
+        setUser({ email, permissions, roles });
+      });
+    }
+  }, []);
 
   const signIn = async ({ email, password }: SignInCredentials) => {
-    console.log({ email, password });
+    try {
+      const response = await api.post("sessions", {
+        email,
+        password,
+      });
+
+      const { token, refreshToken, permissions, roles } = response.data;
+
+      setCookie(undefined, "ignite-next-auth.token", token, {
+        // max time for the cookie
+        maxAge: 60 * 60 * 24 * 30, // 1 month
+        // which paths have access to the cookie
+        path: "/",
+      });
+
+      setCookie(undefined, "ignite-next-auth.refreshToken", refreshToken, {
+        // max time for the cookie
+        maxAge: 60 * 60 * 24 * 30, // 1 month
+        // which paths have access to the cookie
+        path: "/",
+      });
+
+      setUser({
+        email,
+        permissions,
+        roles,
+      });
+
+      api.defaults.headers["Authorization"] = `Bearer ${token}`;
+
+      Router.push("/dashboard");
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
-    <AuthContext.Provider value={{ signIn, isAuthenticated }}>
+    <AuthContext.Provider value={{ signIn, user, isAuthenticated }}>
       {children}
     </AuthContext.Provider>
   );
